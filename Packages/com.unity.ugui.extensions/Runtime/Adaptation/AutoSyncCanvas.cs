461cd531b2ab91880fdbf30520246eb52dd32588
tanweijian <tanweijian199412@gmail.com>
2023-06-16 17:54:43 +0800

    speedtree

---
+++ Packages/com.unity.ugui.extensions/Runtime/Adaptation/AutoSyncCanvas.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace UnityEngine.UI
+{
+    [ExecuteAlways]
+    [RequireComponent(typeof(Canvas))]
+    [RequireComponent(typeof(RectTransform))]
+    public class AutoSyncCanvas : MonoBehaviour
+    {
+        private enum RenderMode
+        {
+            Inherit,
+            World
+        }
+
+        [SerializeField] private RenderMode m_RenderMode;
+
+        private Canvas canvas;
+        private RectTransform rectTransform;
+
+        private void Awake()
+        {
+            canvas = GetComponent<Canvas>();
+            rectTransform = GetComponent<RectTransform>();
+        }
+
+        private void OnEnable()
+        {
+            SyncRectTransform();
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            Awake();
+            SyncRectTransform();
+        }
+
+        private void Update()
+        {
+            SyncRectTransform();
+        }
+#endif
+
+        private void SyncRectTransform()
+        {
+            Canvas rootCanvas = canvas.rootCanvas;
+            if (rootCanvas == null)
+            {
+                return;
+            }
+            RectTransform rootRect = rootCanvas.GetComponent<RectTransform>();
+            switch (m_RenderMode)
+            {
+                case RenderMode.Inherit:
+                    rectTransform.position = rootRect.position;
+                    rectTransform.anchorMin = Vector2.zero;
+                    rectTransform.anchorMax = Vector2.one;
+                    rectTransform.offsetMin = Vector2.zero;
+                    rectTransform.offsetMax = Vector2.zero;
+                    rectTransform.localScale = Vector3.one;
+                    break;
+                case RenderMode.World:
+                    rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+                    rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+                    float rootScale = rootRect.localScale.x;
+                    float scale = 0.01f / rootScale;
+                    rectTransform.localScale = new Vector3(scale, scale, scale);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ Packages/com.unity.ugui.extensions/Runtime/Collections/SerializedDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityEngine.UI
+{
+    [Serializable]
+    public class SerializedDictionary<Tkey, TValue> : Dictionary<Tkey, TValue>, ISerializationCallbackReceiver
+    {
+        [SerializeField] private List<Tkey> m_Keys = new List<Tkey>();
+        [SerializeField] private List<TValue> m_Values = new List<TValue>();
+
+        public void OnBeforeSerialize()
+        {
+            m_Keys.Clear();
+            m_Values.Clear();
+
+            foreach (var kvp in this)
+            {
+                m_Keys.Add(kvp.Key);
+                m_Values.Add(kvp.Value);
+            }
+        }
+
+        public void OnAfterDeserialize()
+        {
+            for (int i = 0; i < m_Keys.Count; i++)
+            {
+                Add(m_Keys[i], m_Values[i]);
+            }
+
+            m_Keys.Clear();
+            m_Values.Clear();
+        }
+    }
+}
