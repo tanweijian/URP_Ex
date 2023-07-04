b23834aa4cb2b057d1074749e68a9d7c99fba875
tanweijian <tanweijian199412@gmail.com>
2023-07-04 18:07:58 +0800

    UISettings

---
+++ Assets/Scripts/BedRock/UI/Runtime/Data/UISettingsAsset.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BedRockRuntime.UI
+{
+    [CreateAssetMenu(fileName = "UISettings", menuName = "BedRock/UI Settings Asset", order = 9999)]
+    public class UISettingsAsset : ScriptableObject
+    {
+        public List<ViewMetaData> ViewMetaDatas;
+    }
+}
